278900e07563ea307db907d3f5928ec75318a9a7
Dmitrii Shelonin <dmitriis@brightdata.com>
2025-11-12 11:38:31 +0200

    - returned branch

--- install_bright_sdk.sh
+++ install_bright_sdk.sh
@@ -2,5 +2,5 @@
 
 name="tmp_install_bright_sdk.sh"
 
-curl -L https://raw.githubusercontent.com/BrightSDK/unity-plugin/refs/heads/mac-integration/install_dependencies.sh -o $name && chmod +x $name && "./$name" && rm $name
-curl -L https://raw.githubusercontent.com/BrightSDK/unity-plugin/refs/heads/mac-integration/install.sh -o $name && chmod +x $name && "./$name" mac-integration && rm $name
+curl -L https://raw.githubusercontent.com/BrightSDK/unity-plugin/refs/heads/main/install_dependencies.sh -o $name && chmod +x $name && "./$name" && rm $name
+curl -L https://raw.githubusercontent.com/BrightSDK/unity-plugin/refs/heads/main/install.sh -o $name && chmod +x $name && "./$name" && rm $name
